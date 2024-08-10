40e04e951c1027dd79bebf0163e8a583228c66aa
hammadam <hammadam@naver.com>
2024-08-10 12:05:53 +0900

    feat: 다시 한 번! "틱택토 게임" 만들기 게임상태와 파생된 상태 작성, 이벤트 핸들러 반환 함수 작성(클로저 활용)

--- 04-tic-tac-toe-game/src/tic-tac-toe/components/Square/Square.jsx
+++ 04-tic-tac-toe-game/src/tic-tac-toe/components/Square/Square.jsx
@@ -1,16 +1,17 @@
-import { node } from 'prop-types';
+import { node, func } from 'prop-types';
 import S from './Square.module.css';
 
 Square.propTypes = {
   children: node,
+  onPlay:func,
 };
 
-function Square({ children }) {
+function Square({ children, onPlay }) {
   // [파생된 상태]
   const isDisabled = !!children;
 
   return (
-    <button className={S.component} disabled={isDisabled}>
+    <button className={S.component} onClick={onPlay} disabled={isDisabled}>
       {children}
     </button>
   );
--- 04-tic-tac-toe-game/src/tic-tac-toe/components/Squares/Squares.jsx
+++ 04-tic-tac-toe-game/src/tic-tac-toe/components/Squares/Squares.jsx
@@ -1,12 +1,35 @@
-import { PLAYER } from '@/tic-tac-toe/constants';
+import { useState } from 'react';
+import { INITIAL_SQUARES, PLAYER, PLAYER_COUNT } from '@/tic-tac-toe/constants';
 import Square from '../Square/Square';
+import S from './Squares.module.css';
 
 function Squares() {
+  // 게임상태
+  const [squares, setSquares] = useState(INITIAL_SQUARES);
+
+  // 게임 상태 업데이트 기능
+  const handlePlay = (index) => () => {
+    console.log(`play game #${index}`);
+    
+  }
+
+  // 게임 파생된 상태
+
+  // 게임 순서
+  const gameIndex = squares.filter(Boolean).length % PLAYER_COUNT;
+
+
+  // 현재 게임 플레이어
+  const currentPlayer = gameIndex === 0 ? PLAYER.ONE : PLAYER.TWO;
+
+
   return (
-    <div className="Squares">
-      <Square></Square>
-      <Square>{PLAYER.ONE}</Square>
-      <Square>{PLAYER.TWO}</Square>
+    <div className={S.component}>
+      {squares.map((square,index)=>{
+        return(
+          <Square key={index} onPlay={handlePlay(index)}></Square>
+        )
+      })}
     </div>
   );
 }
