2bd4e138e3e5215195af99fb6b9744d21bdd0d2d
hammadam <hammadam@naver.com>
2024-08-11 18:26:45 +0900

    feat: 다시 한 번! "틱택토 게임" 만들기
    - 게임 기록 저장
    - 리액트 컴포넌트 상태 및 Game 컴포넌트의 gameHistory, gameIndex 상태

--- 04-tic-tac-toe-game/src/tic-tac-toe/Game.jsx
+++ 04-tic-tac-toe-game/src/tic-tac-toe/Game.jsx
@@ -9,7 +9,10 @@ import History from './components/History/History';
 function Game() {
     // ====== ⚫️🛑⚪️ === 게임상태 === ⚪️🛑⚫️ ======
   // 게임판(9개의 말판) 상태를 나타내는 리액트의 상태 선언
-  const [squares, setSquares] = useState(INITIAL_SQUARES);
+  const [gameHistory, setGameHistory] = useState([INITIAL_SQUARES]);
+
+  // 게임 진행 순서
+  const [gameIndex, setGameIndex] = useState(0);
 
 
 
@@ -24,46 +27,58 @@ function Game() {
       return;
     }
 
+    // 다음 게임의 인덱스는?
+    const nextGemeIndex = gameIndex + 1;
+    
+    //다음 게임 인덱스 상태 업데이트 요청
+    setGameIndex(nextGemeIndex);
+
+    // 게임 히스토리에 기록 추가
+
+
     // 2. 아직 게임이 진행중인 경우
 
     // 아직 진행 중이라면? 게임 진행 (리액트에게 렌더 요청 → 화면 변경)
-    setSquares((prevSquares) => {
-      const nextSquares = prevSquares.map((square, idx) => {
-        return idx === index ? nextPlayer : square;
-      });
-
-      return nextSquares;
+    const nextSquares = currentSquares.map((square, idx) => {
+      return idx === index ? nextPlayer : square;
     });
+
+    const nextGameHistory = [...gameHistory, nextSquares];
+
+    setGameHistory(nextGameHistory);
   };
 
   // ====== ⚫️🛑⚪️ === 게임 파생된 상태 === ⚪️🛑⚫️ ======
 
+  // 게임 히스토리에서 현재 게임판은?
+  const currentSquares = gameHistory[gameIndex];
+
   // 게임이 끝났나 ? 게임 끝났다고 사용자에게 알림 : 반환값에 따라 게임을 진행할지 말지 결정
   // squares?, isPlayOneTurn?, gameIndex?, nextPlayer?
-  const winnerInfo = checkWinner(squares);
+  const winnerInfo = checkWinner(currentSquares);
 
   
     
 
   // 게임 순서 (0, 1, 2, 3, ...)
-  const gameIndex = squares.filter(Boolean).length;   // 0 
+  //const gameIndex = squares.filter(Boolean).length;   // 0 
 
   // 현재 게임 플레이어 ( [0] PLAYER.ONE ↔︎ [1] PLAYER.TWO )
   // 첫번째 플레이어의 턴인가요?
-  const isPlayerOneTurn = gameIndex % PLAYER_COUNT === 0 ; // true
+  const isPlayerOneTurn = currentSquares.filter(Boolean).length % PLAYER_COUNT === 0 ; // true
 
   // 첫번째 플레이어의 턴이라면? PLAYER.ONE : 아니면 PLAYER.TWO
   const nextPlayer = isPlayerOneTurn ? PLAYER.ONE : PLAYER.TWO; // '🍄'
 
   // 게임 상황 체크! (비긴 경우) : 모든 게임판의 말이 채워지고, 승자가 없을 경우
-  const isDraw = !winnerInfo && squares.every(Boolean);
+  const isDraw = !winnerInfo && currentSquares.every(Boolean);
 
 
 
   return (
     <div className={S.component}>
       <Board 
-        squares={squares}
+        squares={currentSquares}
         winnerInfo={winnerInfo}
         nextPlayer={nextPlayer}
         onPlay={handlePlayGame}
