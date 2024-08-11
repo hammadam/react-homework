7ecf6d87717e8b0dcb79765a3bc2e7affd58d8fb
hammadam <hammadam@naver.com>
2024-08-11 16:52:23 +0900

    feat: 다시 한 번! "틱택토 게임" 만들기 ...resProps로 통해 컴포넌트 재사용성 높임

--- 04-tic-tac-toe-game/src/tic-tac-toe/components/Square/Square.jsx
+++ 04-tic-tac-toe-game/src/tic-tac-toe/components/Square/Square.jsx
@@ -1,19 +1,18 @@
-import { node, func, object } from 'prop-types';
+import { node, func } from 'prop-types';
 import S from './Square.module.css';
 
 Square.propTypes = {
   children: node,
-  onPlay:func,
-  style:object
+  onPlay:func
 };
 
 // 상태를 가지지 않는(Stateless) 컴포넌트
-function Square({ children, onPlay, style }) {
+function Square({ children, onPlay, ...resProps }) {
   // [파생된 상태]
   const isDisabled = !!children;
 
   return (
-    <button className={S.component} style={style} onClick={onPlay} disabled={isDisabled}>
+    <button className={S.component} onClick={onPlay} disabled={isDisabled} {...resProps}>
       {children}
     </button>
   );
